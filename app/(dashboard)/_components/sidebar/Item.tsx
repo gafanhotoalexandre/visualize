4946bf5109ca50a6fae55547c3100e6fc87cf39d
Alexandre Vitorino Martins de Lima <alexandrevmartinsdelima@gmail.com>
2024-04-15 11:28:46 -0300

    implementando componente Hint como indicador do Item e NewButton

--- app/(dashboard)/_components/sidebar/Item.tsx
+++ app/(dashboard)/_components/sidebar/Item.tsx
@@ -3,6 +3,7 @@
 import Image from 'next/image'
 import { useOrganization, useOrganizationList } from '@clerk/nextjs'
 
+import { Hint } from '@/components/hint'
 import { cn } from '@/lib/utils'
 
 interface ItemProps {
@@ -24,16 +25,18 @@ export function Item({ id, name, imageUrl }: ItemProps) {
 
   return (
     <div className="aspect-square relative">
-      <Image
-        fill
-        alt={name}
-        src={imageUrl}
-        onClick={onClickOrganization}
-        className={cn(
-          'rounded-md cursor-pointer opacity-75 hover:opacity-100 transition',
-          isActive && 'opacity-100'
-        )}
-      />
+      <Hint label={name} side="right" align="start" sideOffset={18}>
+        <Image
+          fill
+          alt={name}
+          src={imageUrl}
+          onClick={onClickOrganization}
+          className={cn(
+            'rounded-md cursor-pointer opacity-75 hover:opacity-100 transition',
+            isActive && 'opacity-100'
+          )}
+        />
+      </Hint>
     </div>
   )
 }
--- app/(dashboard)/_components/sidebar/NewButton.tsx
+++ app/(dashboard)/_components/sidebar/NewButton.tsx
@@ -3,6 +3,7 @@
 import { Plus } from 'lucide-react'
 import { CreateOrganization } from '@clerk/nextjs'
 
+import { Hint } from '@/components/hint'
 import { Dialog, DialogContent, DialogTrigger } from '@/components/ui/dialog'
 
 export function NewButton() {
@@ -10,9 +11,16 @@ export function NewButton() {
     <Dialog>
       <DialogTrigger asChild>
         <div className="aspect-square">
-          <button className="bg-white/25 h-full w-full rounded-md flex items-center justify-center opacity-60 hover:opacity-100 transition">
-            <Plus className="text-white" />
-          </button>
+          <Hint
+            label="Criar Organização"
+            side="right"
+            align="start"
+            sideOffset={18}
+          >
+            <button className="bg-white/25 h-full w-full rounded-md flex items-center justify-center opacity-60 hover:opacity-100 transition">
+              <Plus className="text-white" />
+            </button>
+          </Hint>
         </div>
       </DialogTrigger>
 
