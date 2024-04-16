2781bf42a02d1b266d144416bf49e84e0b57ee07
Alexandre Vitorino Martins de Lima <alexandrevmartinsdelima@gmail.com>
2024-04-16 11:49:11 -0300

    trabalhando na Navbar

--- app/(dashboard)/_components/Navbar.tsx
+++ app/(dashboard)/_components/Navbar.tsx
@@ -1,16 +1,51 @@
 'use client'
 
-import { UserButton } from '@clerk/nextjs'
+import {
+  OrganizationSwitcher,
+  useOrganization,
+  UserButton,
+} from '@clerk/nextjs'
 
 import { SearchInput } from './SearchInput'
+import { InviteButton } from './InviteButton'
 
 export function Navbar() {
+  const { organization } = useOrganization()
+
   return (
-    <div className="flex items-center-gap-x-4 p-5">
+    <div className="flex items-center gap-x-4 p-5">
       <div className="hidden lg:flex lg:flex-1">
         {/* TODO: Busca */}
         <SearchInput />
       </div>
+
+      <div className="block lg:hidden flex-1">
+        <OrganizationSwitcher
+          hidePersonal
+          appearance={{
+            elements: {
+              rootBox: {
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                width: '100%',
+                maxWidth: '376px',
+              },
+              organizationSwitcherTrigger: {
+                padding: '6px',
+                width: '100%',
+                borderRadius: '8px',
+                border: '1px solid #e5e7eb',
+                justifyContent: 'space-between',
+                backgroundColor: 'white',
+              },
+            },
+          }}
+        />
+      </div>
+
+      {organization && <InviteButton />}
+
       <UserButton afterSignOutUrl="/sign-in" />
     </div>
   )
